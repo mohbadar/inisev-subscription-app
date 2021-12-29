22fce8a6d4c0bb4bd3be167f7370c61b07fe4254
Mohammad Badar Hashimi <32482095+mohbadar@users.noreply.github.com>
2021-12-29 14:02:35 +0430

    mail queueing

--- app/Listeners/SendMailFired.php
+++ app/Listeners/SendMailFired.php
@@ -8,6 +8,7 @@ use App\Models\Post;
 use App\Models\Subscription;
 use App\Models\User;
 use App\Models\Website;
+use Illuminate\Contracts\Mail\MailQueue;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Queue\InteractsWithQueue;
 
@@ -34,38 +35,45 @@ class SendMailFired
      */
     public function handle(SendMail $event)
     {
-
-        // dd($event);
-
         $subscriptions = $event->subscriptions;
 
-        foreach($subscriptions as $subscription){
+        if($subscriptions){
+            foreach($subscriptions as $subscription){
 
-            $user = User::find($subscription->user_id)->toArray();
-            $website = Website::find($subscription->website_id)->toArray();
+                $user = User::find($subscription->user_id)->toArray();
+                $website = Website::find($subscription->website_id)->toArray();
 
-            $posts = $website->posts();
+                // send email for the posts belongs to this website
+                $posts = $website->posts();
 
-            foreach($posts as $post){
+                foreach($posts as $post){
+                    //get send emails to user
+                    $mails = $user->mails();
+                    foreach($mails as $mail){
+                        //send email if already not sent
+                        if($post->id != $mail->post_id){
+                            // To avoid error Mail server need to be configured
+                            print($post);
+                            //tested mail sending implementation
+                            // Mail::to($user->email)->send(new Subscribe($user->email, $post->title, $post->description));
 
-
-                //get send emails to user
-                $mails = $user->mails();
-                foreach($mails as $mail){
-                    //send email if already not sent
-                    if($post->id != $mail->post_id){
-                        // To avoid error Mail server need to be configured
-                        Mail::to($user->email)->send(new Subscribe($user->email, $post->title, $post->description));
+                            // or  user MailQueue for queueing mail sending
+                            // Mail::queue('Html.view', $data, function ($message) {
+                            //     $message->from('john@johndoe.com', 'John Doe');
+                            //     $message->sender('john@johndoe.com', 'John Doe');
+                            //     $message->to('john@johndoe.com', 'John Doe');
+                            //     $message->cc('john@johndoe.com', 'John Doe');
+                            //     $message->bcc('john@johndoe.com', 'John Doe');
+                            //     $message->replyTo('john@johndoe.com', 'John Doe');
+                            //     $message->subject('Subject');
+                            //     $message->priority(3);
+                            //     $message->attach('pathToFile');
+                            // });
+                        }
                     }
                 }
 
-
             }
-
         }
-
     }
-
-
-
 }
